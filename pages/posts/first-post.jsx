1bda87f34e395318e3bba5200841f0ea7957cf77
Wallace <wallacelim97@gmail.com>
2020-05-08 09:00:06 +0800

    Implement basic demo page

---
+++ pages/posts/first-post.jsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Link from "next/Link";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>My First Post</h1>
+
+            <Link href="/">
+                <a>Back to home</a>
+            </Link>
+        </Layout>
+    );
+}
